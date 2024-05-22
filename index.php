befd2889411e025df0a5d92aa64a90fd8e10642b
Jon Tice <chilifreez@aol.com>
2024-05-21 20:57:58 -0400

    Small CSS Changes to existing pages

--- index.php
+++ index.php
@@ -61,7 +61,7 @@
 
       $('.navButtonContainer, .logo').click(function(e) {
         var url = $(this).attr('id') + ".html";
-        //console.log(url);
+        console.log(url);
         if (url != "contact.html") {
 
           e.preventDefault();
